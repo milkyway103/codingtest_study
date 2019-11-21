ed823e4fbfbd7bc44c964fed30fae7c645b585b2
eunha <1002galaxy@gmail.com>
2019-11-21 17:52:13 +0900

    ch07_1_ex_java

--- jongmanbook/ch7/ch07_ex.java
+++ jongmanbook/ch7/ch07_ex.java
@@ -1,5 +1,6 @@
 package com.milkyway.jongman;
 
+import java.util.List;
 import java.util.ArrayList;
 
 public class ch07_ex {
@@ -61,6 +62,51 @@ public class ch07_ex {
 		}
 		c = normalize(c);
 		return c;
+	}
+	
+	public static ArrayList<Integer> addTo(ArrayList<Integer> a, ArrayList<Integer> b, int k) {
+		// a+= b*(10^k);를 구현
+		return a;
+	}
+
+	public static ArrayList<Integer> subFrom(ArrayList<Integer> a, ArrayList<Integer> b) {
+		// a-= b;를 구현. a>=b를 가정
+		return a;
+	}
+	
+	// 두 긴 정수의 곱을 반환.
+	public static ArrayList<Integer> karatsuba(ArrayList<Integer> a, ArrayList<Integer> b) {
+		int an = a.size();
+		int bn = b.size();
+		// a가 b보다 짧을 경우 둘을 바꾼다.
+		if(an < bn) return karatsuba(b, a);
+		// 기저 사례 : a나 b가 비어 있는 경우
+		if(an == 0 || bn == 0) return new ArrayList<Integer>();
+		// 기저 사례: a가 비교적 짧은 경우 O(n^2) 곱셈으로 변경.
+		if (an <= 50) return multiply(a, b);
+		int half = an / 2;
+		// a와 b를 밑에서 half 자리와 나머지로 분리한다.
+		ArrayList<Integer> a0 = new ArrayList(a.subList(0, half));
+		ArrayList<Integer> a1 = new ArrayList(a.subList(half, a.size()));
+		ArrayList<Integer> b0 = new ArrayList(b.subList(0, Math.min(b.size(), half)));
+		ArrayList<Integer> b1 = new ArrayList(b.subList(Math.min(b.size(), half), b.size()));
+ 		// z2 = a1*b1
+		ArrayList<Integer> z2 = karatsuba(a1, b1);
+		// z0 = a0*b0
+		ArrayList<Integer> z0 = karatsuba(a0, b0);
+		// a0 = a0+a1, b0 = b0+b1
+		a0 = addTo(a0, a1, 0);
+		b0 = addTo(b0, b1, 0);
+		// z1 = (z0*b0) - z0 - z2;
+		ArrayList<Integer> z1 = karatsuba(a0, b0);
+		z1 = subFrom(z1, z0);
+		z1 = subFrom(z1, z2);
+		// ret = z0 + z1 * 10^half + z2 * 10^(half*2)
+		ArrayList<Integer> ret = new ArrayList<Integer>();
+		ret = addTo(ret, z0, 0);
+		ret = addTo(ret, z1, half);
+		ret = addTo(ret, z2, half + half);
+		return ret;
 	}
 	
 
