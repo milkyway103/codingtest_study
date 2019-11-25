fbb83b9dfceb442943831f8f42afe17260b020c1
eunha <1002galaxy@gmail.com>
2019-11-25 15:55:07 +0900

    카카오인턴문제 4, 5번 주석 추가 및 코드 수정

--- programmers/kakao2020인턴/kakaointern04.py
+++ programmers/kakao2020인턴/kakaointern04.py
@@ -34,27 +34,47 @@ k	room_number	result
 
 첫 번째 ~ 세 번째 고객까지는 원하는 방이 비어 있으므로 즉시 배정받을 수 있습니다. 네 번째 고객의 경우 1번 방을 배정받기를 원했는데, 1번 방은 빈 방이 아니므로, 1번 보다 번호가 크고 비어 있는 방 중에서 가장 번호가 작은 방을 배정해야 합니다. 1번 보다 번호가 크면서 비어있는 방은 [2번, 5번, 6번...] 방이며, 이중 가장 번호가 작은 방은 2번 방입니다. 따라서 네 번째 고객은 2번 방을 배정받습니다. 마찬가지로 5, 6번째 고객은 각각 5번, 6번 방을 배정받게 됩니다.
 '''
+
+
 def solution(k, room_number):
+    '''
+    효율성의 핵심은 같은 data를 여러 번 보지 않기
+    이 문제에서는 이미 있는 것으로 확인된 room을 요청이 들어올 때마다 계속 확인한다면 비효율!
+    '''
     answer = []
-    nextroom = {}
+    nextroom = {} # key : 요청된 방 번호 / value : 요청이 들어온 방에 대해 가장 먼저 비어있는지 볼 방 번호
     for room in room_number:
+        # 만약 room이 nextroom의 key에 있다면 이미 차 있는 방이라는 뜻
         if room in nextroom.keys():
-            while True:
-                temp = nextroom[room]
-                nextroom[room] = nextroom[nextroom[room]]
+            # udpatenextroom함수로 보내 다음 방을 배정받도록 한다.
+            answer.append(updatenextroom(nextroom, room))
         else:
-            nextroom[room] = room
-        answer.append(nextroom[room])
-        temp = nextroom[room]
-        if temp == room:
-            temp += 1
-        while True:
-            if temp in nextroom.keys():
-                temp = nextroom[temp]
-            else:
-                nextroom[room] = temp
-                break
+            # 없다면 배정하고
+            answer.append(room)
+            # nextroom dictionary에 다음으로 안내될 방 번호 (room+1)을 저장해 둔다.
+            # 이 room+1가 이미 차 있는 방이라도 updatenextroom function에서 한 번 더 확인하기 때문에 괜찮다.
+            nextroom[room] = room+1
         print(answer, nextroom)
     return answer
 
-solution(10, [1, 3, 4, 1, 3, 1, [1, 3, 4, 2, 5, 6]])
+def updatenextroom(nextroom, roomnum):
+    # 비어 있는 room을 만날 때까지 nextroom을 타고 간다.
+    # 이 때 이미 확인한 방은 건너뛰고 다음으로 확인할 비어 있는 방이 계속 value로 update되기 때문에
+    # 한 번 차 있는 것을 확인한 방은 다시 보지 않는다.
+    # 혹은 한 번 배정된 방의 value는 그 방 +1의 값으로 저장된다.
+    # -> 이 방은 이미 차 있으므로 다음에 들어온 고객이 이 방을 원할 경우, 이 방 (key)에 저장된 방 (value)부터 비어 있는지 확인하시오.
+    if nextroom[roomnum] not in nextroom.keys():
+        ans = nextroom[roomnum]
+        # roomnum, ans의 nextroom을 update해준다.
+        nextroom[roomnum] += 1
+        # 빈 방을 찾으면 그 방에 대해서도 ans, ans+1로 저장해준다.
+        nextroom[ans] = ans+1
+        return ans
+    else:
+        nextroom[roomnum] = nextroom[nextroom[roomnum]]
+        return updatenextroom(nextroom, roomnum)
+
+# ans + 1, room + 1이 비어있다는 확신은 없지만, 저장할 때마다 계속 연쇄적으로 빈 방을 찾아서 update하는 것보다
+# 요청이 들어올 때마다 update하는 것이 효율이 좋다.
+
+solution(10, [1, 3, 4, 1, 3, 1])
--- programmers/kakao2020인턴/kakaointern05.py
+++ programmers/kakao2020인턴/kakaointern05.py
@@ -1,23 +1,37 @@
 def solution(stones, k):
+    # k, 즉 한 번에 뛰어넘을 수 있는 돌의 개수가 1인 경우에는 돌들 중 하나라도 0이 되면 건너갈 수 없게 되므로
+    # stones의 최솟값을 return한다.
     if k == 1:
         return min(stones)
 
-    answer = float('inf')
+    # 그 외에는 k개의 돌들 중 가장 큰 값의 최솟값을 return
+
+    # premaxidx와 answer의 첫 번째 stone으로 초기화
     premaxidx = 0
+    answer = stones[premaxidx]
+
+    # 첫 k개까지의 돌들 중에서 max값인 stone의 idx를 premaxidx에 저장
     for idx in range(1, k):
         if stones[premaxidx] <= stones[idx]:
             premaxidx = idx
+            # 이때 answer도 함께 update
             answer = stones[premaxidx]
 
+    # 모든 stones를 돌면서
     for idx in range(1, len(stones)):
         if idx + k > len(stones):
             break
+        # 이미 확인한 idx라면 건너뛴다.
         if premaxidx == idx:
             continue
+        # 지금 확인하고 있는 idx가 premaxidx보다 더 작고 (앞의 인덱스이고)
+        # stone의 수가 같거나 크다면
+        # premaxidx와 answer를 update하고
         if premaxidx > idx:
             if stones[premaxidx] <= stones[idx]:
                 premaxidx = idx
                 answer = min(stones[premaxidx], answer)
+        # 지금 확인하고 있는 idx가 premaxidx보다 같거나 크다면 premaxidx를 다시 계산하여 update해야 한다.
         else:
             premaxidx = idx
             for jdx in range(idx + 1, idx + k):
